6a90a6114b4ed0a144545dc59f86cb7f0ae77c11
SOTOMURA Natsuko <sotomura@pxgrid.com>
2016-12-12 21:54:28 +0900

    add demo for 6th

---
+++ 6/memo/src/page/Add.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <editor-view @add="add"></editor-view>
+  </div>
+</template>
+
+<script lang="babel">
+  import EditorView from '../components/EditorView'
+  export default{
+    methods: {
+      add(data) {
+        this.$emit('add', data)
+        this.$router.push({name: 'items'})
+      }
+    },
+    components: {
+      EditorView
+    }
+  }
+</script>
---
+++ 6/memo/src/page/Root.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <list-view :memos="memos" @remove="remove" :count="3" sort="latest"></list-view>
+    <router-link :to="{name: 'items'}">すべて見る</router-link>
+  </div>
+</template>
+
+<script lang="babel">
+  import ListView from '../components/ListView'
+  export default{
+    props: {
+      memos: Array
+    },
+    methods: {
+      remove(id) {
+        this.$emit('remove', id)
+      }
+    },
+    components: {
+      ListView
+    }
+  }
+</script>
+

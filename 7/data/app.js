0aa5792aacd02eaf74bfeafb0e9e7eb5baabc58f
nakajmg <nakajima.jmg@gmail.com>
2017-04-14 08:20:13 +0900

    即時関数で囲む

--- 7/data/app.js
+++ 7/data/app.js
@@ -1,37 +1,41 @@
-const dataSource = {
-  message: 'Hello Vue.js!!'
-}
+(function() {
 
-const vm1 = new Vue({
-  el: '#vm1',
-  template: `
-    <div> 
-      <input v-model="message">
-    </div>
-  `,
-  data() {
-    return dataSource
+  const dataSource = {
+    message: 'Hello Vue.js!!'
   }
-})
 
-const vm2 = new Vue({
-  el: '#vm2',
-  template: `
-    <div>
-      <p>{{message}}</p>
-      <button @click="clear">clear</button>
-    </div>
-  `,
-  data() {
-    return dataSource
-  },
-  methods: {
-    clear() {
-      this.message = ''
+  const vm1 = new Vue({
+    el: '#vm1',
+    template: `
+      <div> 
+        <input v-model="message">
+      </div>
+    `,
+    data() {
+      return dataSource
     }
-  }
-})
+  })
 
-document.querySelector('#button').addEventListener('click', (e) => {
-  dataSource.message += 'button clicked!!'
-})
+  const vm2 = new Vue({
+    el: '#vm2',
+    template: `
+      <div>
+        <p>{{message}}</p>
+        <button @click="clear">clear</button>
+      </div>
+    `,
+    data() {
+      return dataSource
+    },
+    methods: {
+      clear() {
+        this.message = ''
+      }
+    }
+  })
+
+  document.querySelector('#button').addEventListener('click', (e) => {
+    dataSource.message += 'button clicked!!'
+  })
+
+})()
--- 7/store/app.js
+++ 7/store/app.js
@@ -1,38 +1,40 @@
-const vm1 = new Vue({
-  el: '#vm1',
-  template: `
-    <div> 
-      <input :value="sharedState.message" @input="updateMessage">
-    </div>
-  `,
-  data() {
-    return {
-      sharedState: store.state
+(function() {
+  const vm1 = new Vue({
+    el: '#vm1',
+    template: `
+      <div> 
+        <input :value="sharedState.message" @input="updateMessage">
+      </div>
+    `,
+    data() {
+      return {
+        sharedState: store.state
+      }
+    },
+    methods: {
+      updateMessage(e) {
+        store.setMessageAction(e.target.value)
+      }
     }
-  },
-  methods: {
-    updateMessage(e) {
-      store.setMessageAction(e.target.value)
-    }
-  }
-})
+  })
 
-const vm2 = new Vue({
-  el: '#vm2',
-  template: `
-    <div>
-      <p>{{sharedState.message}}</p>
-      <button @click="clear">clear</button>
-    </div>
-  `,
-  data() {
-    return {
-      sharedState: store.state
+  const vm2 = new Vue({
+    el: '#vm2',
+    template: `
+      <div>
+        <p>{{sharedState.message}}</p>
+        <button @click="clear">clear</button>
+      </div>
+    `,
+    data() {
+      return {
+        sharedState: store.state
+      }
+    },
+    methods: {
+      clear() {
+        store.clearMessageAction()
+      }
     }
-  },
-  methods: {
-    clear() {
-      store.clearMessageAction()
-    }
-  }
-})
+  })
+})()
